a71822bd5ca47f533c6103d0b6215cfe78b7b12c
thadeus <thadeusnyariki94@gmail.com>
2022-11-06 13:15:36 +0300

    Done assignemnt

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,3 +2,10 @@ require 'nokogiri'
 require 'open-uri'
 
 html = open("https://flatironschool.com/")
+
+doc = Nokogiri::HTML(html)
+courses = doc.css(".card-body.card-pad-1g")
+
+courses.each do |course|
+    puts course.text.strip
+end
